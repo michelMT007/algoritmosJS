a8c46352a3dec3e1f9175bca9648efc5249863ce
michelMT007 <michelifmtads@gmail.com>
2021-02-18 21:53:55 -0400

    add algoritmo AlunosPresentes correção

--- modulo1/alunosPreFalta.js
+++ modulo1/alunosPreFalta.js
@@ -1,10 +1,8 @@
 var nomesDosPresentes = ["Talissa", "Raissa", "Joao", "Peter", "Paul", "Brian", "Suse", "Ubuntu", "Lars", "Denni", "Robert"];
-
 function estavaPresenteNaAula(nomeDoAluno, nomesDosPresentes) {
     var retorno = false;
     var alunoPresente = "";
     var alunoFaltou = "";
-
     if (nomesDosPresentes.indexOf(nomeDoAluno) >= 0) {
         alunoPresente = nomesDosPresentes[nomesDosPresentes.indexOf(nomeDoAluno)];
         console.log(alunoPresente + " esta presente na aula!");
@@ -14,8 +12,6 @@ function estavaPresenteNaAula(nomeDoAluno, nomesDosPresentes) {
         console.log(alunoFaltou + " ausente na aula!");
         retorno = false;
     }
-
     return retorno;
-
 }
-estavaPresenteNaAula("Taliss", nomesDosPresentes);
+estavaPresenteNaAula("Talissa", nomesDosPresentes);
