ac8d4b89b71acb486d38d0173891bdd009035b84
michelMT007 <michelifmtads@gmail.com>
2021-02-18 21:59:55 -0400

    add algoritmo encontrar string na lista Array

---
+++ modulo1/encontrarrei.js
@@ -0,0 +1,12 @@
+var baralho = ["Ás", "Dama", "Rei", "Valete"]
+  
+//Seu código aqui
+function encontrarRei(){
+    for(var i =0; i < baralho.length; i++){
+        if(baralho[i]=="Rei"){
+            console.log("Encontrei o "+baralho[i]+"!");
+            break
+        }
+    }
+}
+encontrarRei();
