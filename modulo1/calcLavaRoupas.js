2f0ac3a83e1f0f2bccb62d26a328d5be4e15bfff
michelMT007 <michelifmtads@gmail.com>
2021-02-18 21:55:32 -0400

    add algoritmo Calcula pagamento Lavanderia

---
+++ modulo1/calcLavaRoupas.js
@@ -0,0 +1,13 @@
+function calculaValorDevido(pesoDeRoupaSuja){
+    var valorcorreto;
+    if(pesoDeRoupaSuja <1){
+        valorcorreto=(10.00);
+        console.log(valorcorreto);
+    }else{
+        valorcorreto=((pesoDeRoupaSuja*3.00)+10.00);
+        console.log(valorcorreto);
+    }
+    return valorcorreto;
+
+}
+calculaValorDevido(5);
